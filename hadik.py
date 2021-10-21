a074158f7ae8989360f80e20794dc8244790f629
BelKed <66956532+BelKed@users.noreply.github.com>
2021-10-21 21:22:00 +0200

    Change theme colors

--- hadik.py
+++ hadik.py
@@ -1,14 +1,12 @@
 import tkinter
 import time
 
-width = 800
-height = 800
+width = 720
+height = 720
 x, y, r = width/2, height/2, 20
 
-pl = tkinter.Canvas(bg='gray', width=width, height=height)
+pl = tkinter.Canvas(bg="#282c34", width=width, height=height)
 pl.pack()
-
-pl.create_rectangle(x - r, y - r, x + r, y + r, fill='yellow', width=0)
 
 coords = [[x, y]]
 
@@ -20,56 +18,56 @@ def kresli(event):
     prev_x = x
     prev_y = y
 
-    if tl == 'Left':
+    if tl == "Left":
         x -= r*2
-    elif tl == 'Right':
+    elif tl == "Right":
         x += r*2
-    elif tl == 'Up':
+    elif tl == "Up":
         y -= r*2
-    elif tl == 'Down':
+    elif tl == "Down":
         y += r*2
 
     if (x + r) > width:
         x -= 2*r
-        oval(prev_x, prev_y, 'red')
+        oval(prev_x, prev_y, "#e74c3c")
         pl.update()
         time.sleep(0.2)
-        oval(x, y, 'orange')
+        oval(x, y, "#76e2af")
         pl.update()
     elif (x - 2*r) < 0:
         x += 2*r
-        oval(prev_x, prev_y, 'red')
+        oval(prev_x, prev_y, "#e74c3c")
         pl.update()
         time.sleep(0.2)
-        oval(x, y, 'orange')
+        oval(x, y, "#76e2af")
         pl.update()
 
     elif (y + 2*r) > height:
         y -= 2*r
-        oval(prev_x, prev_y, 'red')
+        oval(prev_x, prev_y, "#e74c3c")
         pl.update()
         time.sleep(0.2)
-        oval(x, y, 'orange')
+        oval(x, y, "#76e2af")
         pl.update()
     elif (y - 2*r) < 0:
         y += 2*r
-        oval(prev_x, prev_y, 'red')
+        oval(prev_x, prev_y, "#e74c3c")
         pl.update()
         time.sleep(0.2)
-        oval(x, y, 'orange')
+        oval(x, y, "#76e2af")
         pl.update()
 
     else:
-        oval(prev_x, prev_y, 'yellow')
+        oval(prev_x, prev_y, "#32b1d3")
         if ([x, y] in coords):
-            oval(x, y, 'red')
+            oval(x, y, "#e74c3c")
             pl.update()
             time.sleep(0.2)
-            oval(x, y, 'blue')
+            oval(x, y, "#76e2af")
             pl.update()
         else:
-            oval(x, y, 'orange')
-            oval(prev_x, prev_y, 'yellow')
+            oval(x, y, "#76e2af")
+            oval(prev_x, prev_y, "#32b1d3")
             coords.append([x, y])
 
 
@@ -77,6 +75,7 @@ def oval(x_oval, y_oval, fill):
     pl.create_rectangle(x_oval - r, y_oval - r, x_oval + r, y_oval + r, fill=fill, width=0)
 
 
-pl.bind_all('<Key>', kresli)
+oval(x, y, "#76e2af")
+pl.bind_all("<Key>", kresli)
 
 pl.mainloop()
