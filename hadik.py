035c8926523bfe242f7311bd62649b7997cd0895
BelKed <66956532+BelKed@users.noreply.github.com>
2021-10-21 18:42:00 +0200

    Add snake's head and change color back after collision

--- hadik.py
+++ hadik.py
@@ -16,25 +16,29 @@ def kresli(event):
 
     tl = event.keysym
 
+    prev_x = x
+    prev_y = y
+
     if tl == 'Left':
         x -= r*2
 
-    if tl == 'Right':
+    elif tl == 'Right':
         x += r*2
 
-    if tl == 'Up':
+    elif tl == 'Up':
         y -= r*2
 
-    if tl == 'Down':
+    elif tl == 'Down':
         y += r*2
 
-    color = 'yellow'
+    color = 'red'
     if ([x, y] in coords):
         color = 'blue'
     else:
         coords.append([x, y])
 
     pl.create_oval(x - r, y - r, x + r, y + r, fill=color, outline='red', width=5)
+    pl.create_oval(prev_x - r, prev_y - r, prev_x + r, prev_y + r, fill='yellow', outline='red', width=5)
 
 
 pl.bind_all('<Key>', kresli)
