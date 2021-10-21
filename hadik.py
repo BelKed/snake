3dc7f778f637214426d57c4bd9207be0728da226
BelKed <66956532+BelKed@users.noreply.github.com>
2021-10-21 18:37:00 +0200

    Change color on collision with tail

--- hadik.py
+++ hadik.py
@@ -1,5 +1,3 @@
-# dokoncit hadika (prechod -> zmena kruzku)
-
 import tkinter
 
 pl = tkinter.Canvas(bg='blue', width=800, height=600)
@@ -9,15 +7,14 @@ xt, yt = 400, 50
 x, y, r = 400, 300, 10
 
 pl.create_oval(x - r, y - r, x + r, y + r, fill='yellow', outline='red', width=5)
-pl.create_text(xt, yt, fill='yellow', text='KUK', font='Arial 30', angle=45)
+
+coords = [[x, y]]
+
 
 def kresli(event):
     global x, y, r
 
-    # pl.delete('all')
-
     tl = event.keysym
-    print(tl)
 
     if tl == 'Left':
         x -= r*2
@@ -31,7 +28,14 @@ def kresli(event):
     if tl == 'Down':
         y += r*2
 
-    pl.create_oval(x - r, y - r, x + r, y + r, fill='yellow', outline='red', width=5)
+    color = 'yellow'
+    if ([x, y] in coords):
+        color = 'blue'
+    else:
+        coords.append([x, y])
+
+    pl.create_oval(x - r, y - r, x + r, y + r, fill=color, outline='red', width=5)
+
 
 pl.bind_all('<Key>', kresli)
 
