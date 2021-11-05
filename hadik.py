7f7d038de824183c15042302edbd5172dd581a58
BelKed <66956532+BelKed@users.noreply.github.com>
2021-11-05 19:03:00 +0100

    Format text and rename variables and functions

--- hadik.py
+++ hadik.py
@@ -1,19 +1,16 @@
 import tkinter
 import time
 
-width = 720
-height = 720
-x, y = width/2, height/2
-
 size = 20
 max_lenght = 10
+canvas_size = 720
 
 red, green, blue, gray = "#e36666", "#76e2af", "#32b1d3", "#282c34"
-
-pl = tkinter.Canvas(bg=gray, width=width, height=height)
-pl.pack()
-
+x = y = canvas_size / 2
 coords = [[x, y]]
+
+pl = tkinter.Canvas(bg=gray, width=canvas_size, height=canvas_size)
+pl.pack()
 
 
 def draw(event):
@@ -22,56 +19,58 @@ def draw(event):
     tl = event.keysym
     prev_x, prev_y = x, y
 
-    if tl == "Left": x -= size*2
-    elif tl == "Right": x += size*2
-    elif tl == "Up": y -= size*2
-    elif tl == "Down": y += size*2
+    if tl == "Left": x -= size * 2
+    elif tl == "Right": x += size * 2
+    elif tl == "Up": y -= size * 2
+    elif tl == "Down": y += size * 2
     else: return
 
-    if (x + size) > width:
-        x -= size*2
+    if (x + size) > canvas_size:
+        x -= size * 2
         blink(x, y)
-    elif (x - size*2) < 0:
-        x += size*2
+    elif (x - size * 2) < 0:
+        x += size * 2
         blink(x, y)
 
-    elif (y + size*2) > height:
-        y -= size*2
+    elif (y + size * 2) > canvas_size:
+        y -= size * 2
         blink(x, y)
-    elif (y - size*2) < 0:
-        y += size*2
+    elif (y - size * 2) < 0:
+        y += size * 2
         blink(x, y)
 
     else:
-        oval(prev_x, prev_y, blue)
+        square(prev_x, prev_y, blue)
 
         if [x, y] in coords:
-            if tl == "Left": x += size*2
-            elif tl == "Right": x -= size*2
-            elif tl == "Up": y += size*2
-            elif tl == "Down": y -= size*2
+            if tl == "Left": x += size * 2
+            elif tl == "Right": x -= size * 2
+            elif tl == "Up": y += size * 2
+            elif tl == "Down": y -= size * 2
 
             blink(x, y)
         else:
             coords.append([x, y])
-            oval(x, y, green)
+            square(x, y, green)
 
         if len(coords) > max_lenght:
-            oval(coords[0][0], coords[0][1], gray)
-            coords.remove(coords[0])
+            square(coords[0][0], coords[0][1], gray)
+            coords.pop(0)
 
 
-def oval(x_oval, y_oval, fill):
-    pl.create_rectangle(x_oval - size, y_oval - size, x_oval + size, y_oval + size, fill=fill, width=0)
+def square(_x, _y, fill):
+    pl.create_rectangle(_x - size, _y - size, _x + size, _y + size, fill=fill, width=0)
 
-def blink(x_oval, y_oval):
-    oval(x_oval, y_oval, red)
+
+def blink(_x, _y):
+    square(_x, _y, red)
     pl.update()
     time.sleep(0.1)
-    oval(x_oval, y_oval, green)
+    square(_x, _y, green)
     pl.update()
 
-oval(x, y, green)
+
+square(x, y, green)
 pl.bind_all("<Key>", draw)
 
 pl.mainloop()
